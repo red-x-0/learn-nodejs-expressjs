a66474512bb2db5a1106a45dc585184d5a52c27e
亗RX | Sief <sief4136@gmail.com>
2024-06-20 00:24:06 +0300

    added some comments

--- routes/main.js
+++ routes/main.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const { main_index_get, user_index_post} = require("../controllers/mainController.js")
 // PATH '/'
 
+/**
+* @route /
+* @method GET
+* @access public
+*/
 router.get("/", main_index_get);
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -3,16 +3,46 @@ const router = express.Router();
 const { user_view_get, user_add_get, user_add_post, user_edit_get, user_edit_post, user_delete } = require("../controllers/userController.js")
 // PATH '/user'
 
+/**
+* @route /user/:id
+* @method GET
+* @access public
+*/
 router.get("/:id", user_view_get);
 
+/**
+* @route /user/add
+* @method GET
+* @access public
+*/
 router.get("/add", user_add_get);
 
+/**
+* @route /user/add
+* @method POST
+* @access public
+*/
 router.post("/add", user_add_post);
 
+/**
+* @route /user/edit/:id
+* @method GET
+* @access public
+*/
 router.get("/edit/:id", user_edit_get);
 
+/**
+* @route /user/edit/:id
+* @method POST
+* @access public
+*/
 router.post("/edit/:id", user_edit_post);
 
+/**
+* @route /user/delete/:id
+* @method DELETE
+* @access public
+*/
 router.delete("/delete/:id", user_delete);
 
 module.exports = router;
